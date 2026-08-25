08181ca6f156146e9fbb90c0da52d070b6ca0c84
Servio Palacios <servio@palacios.com>
2026-08-25 10:35:27 -0600

    [cleanup script]

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,5 +4,5 @@ sudo pkill -f 'suricata.*suricata-lab' 2>/dev/null || true
 sudo pkill -f 'python3 -m http.server 8080' 2>/dev/null || true
 sudo pkill arpspoof 2>/dev/null || true
 sudo mn -c
-echo "Entorno Mininet limpiado."
+echo "Entorno Mininet limpio."
 
